273edd8ddd1da7203b10d67c402de29c5801a19c
mcollcarmona-hue <mcollcarmona@gmail.com>
2026-09-08 13:49:22 +0200

    Update expiration date to December 31, 2026

--- app (2).py
+++ app (2).py
@@ -19,7 +19,7 @@ st.set_page_config(
 )
 
 APP_DIR = Path(__file__).parent
-FECHA_CADUCIDAD = date.fromisoformat("2026-08-04")
+FECHA_CADUCIDAD = date.fromisoformat("2026-12-31")
 
 hoy = datetime.today().date()
 
