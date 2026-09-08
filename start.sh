9a664f862dd19006a217b21bac3913be81245f2a
mcollcarmona-hue <mcollcarmona@gmail.com>
2026-09-08 18:17:53 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-npm install -g localtunnel
-echo "=== TU CONTRASEÑA DE LOCALTUNNEL (ENDPOINT IP) ES: ==="
-curl -s ipv4.icanhazip.com
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0 --server.enableCORS false --server.enableXsrfProtection false & npx localtunnel --port 8501 --subdomain tfm-pricing-mcoll
